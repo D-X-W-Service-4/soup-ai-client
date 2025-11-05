dd87e7f5c9262291bcc8126957212ba930af67c4
yunjaeekim <kyj9396922@gmail.com>
2025-11-05 17:25:59 +0900

    Add OCR, Scroing with Vast ai server(Connect with HTTP)

--- api/v1/routers.py
+++ api/v1/routers.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
-from api.v1.endpoints import planner, quiz
+from api.v1.endpoints import planner, quiz, ocr, grade
 
 api_router = APIRouter()
 
 api_router.include_router(planner.router, prefix="/planner", tags=["planner"])
 api_router.include_router(quiz.router, prefix="/quiz", tags=["quiz"])
+api_router.include_router(ocr.router, prefix="/ocr", tags=["ocr"])  
+api_router.include_router(grade.router, prefix="/grade", tags=["grade"])
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,7 @@
 from utils.db_utils import *
 from utils.llm_utils import *
+from utils.ocr_utils import *
+from utils.grade_utils import *
 from utils.graph_utils import get_avg_quiz_score, extract_accuracy_by_topic, extract_accuracy_by_difficulty
 
 __all__ = [
@@ -12,6 +14,8 @@ __all__ = [
     "ensure_json",
     "get_avg_quiz_score",
     "extract_accuracy_by_topic",
-    "extract_accuracy_by_difficulty"
-    
+    "extract_accuracy_by_difficulty",
+    "imagefile_to_b64_png",
+    "call_kanana_generate",
+    "call_exaone_async",
 ]
